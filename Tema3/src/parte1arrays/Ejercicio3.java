5874c917d00ee3167ab49a51d382f6d75502686f
HugoDominguezF <hugo.dominguez@iesnervion.es>
2024-11-08 14:39:49 +0100

    ejercicios

--- Tema3/src/parte1arrays/Ejercicio3.java
+++ Tema3/src/parte1arrays/Ejercicio3.java
@@ -4,7 +4,11 @@ public class Ejercicio3 {
 
 	public static void main(String[] args) {
 		
-
+		int tabla[] = new int[10];
+			
+		
+		
+		
 	}
 
 }
---
+++ Tema3/src/parte2clasearrays/Ejercicio1.java
@@ -0,0 +1,22 @@
+package parte2clasearrays;
+
+import java.util.Scanner;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		sc.close();
+	}
+
+}
