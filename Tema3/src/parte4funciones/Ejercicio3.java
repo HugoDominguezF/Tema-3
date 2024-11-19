007eff1b87402b9cce91b3e9e9c8dcf0ac70a127
HugoDominguezF <hugo.dominguez@iesnervion.es>
2024-11-19 14:42:31 +0100

    ejercicios

--- Tema3/src/parte4funciones/Ejercicio3.java
+++ Tema3/src/parte4funciones/Ejercicio3.java
@@ -7,40 +7,40 @@ import java.util.Scanner;
 public class Ejercicio3 {
 
 	public static void main(String[] args) {
-		
-		Scanner sc=new Scanner(System.in);
-		
+
+		Scanner sc = new Scanner(System.in);
+
 		int longitud;
 		int fin;
-		
+
 		System.out.println("Diga la longitud de la tabla");
-		longitud= sc.nextInt();
-		
+		longitud = sc.nextInt();
+
 		System.out.println("Digame el numero final del intervalo aleatorio");
-		fin=sc.nextInt();
-		
+		fin = sc.nextInt();
+
 		int resultado[] = rellenaPares(longitud, fin);
-		
+
 		System.out.println(Arrays.toString(resultado));
-		
+
 		sc.close();
-		
+
 	}
 
-	public static int[] rellenaPares(int longitud, int fin) {
-		
+	static int[] rellenaPares(int longitud, int fin) {
+
 		Random rand = new Random();
-		
-		int tabla[]=new int[longitud];
-		
-		for(int contador=0;contador<tabla.length;contador++) {
+
+		int tabla[] = new int[longitud];
+
+		for (int contador = 0; contador < tabla.length; contador++) {
 			do {
-			tabla[contador] = rand.nextInt(2,fin);
-			}while(tabla[contador]%2!=0);
+				tabla[contador] = rand.nextInt(2, fin);
+			} while (tabla[contador] % 2 != 0);
 		}
-		
+
 		Arrays.sort(tabla);
-		
-		return(tabla);
+
+		return (tabla);
 	}
 }
---
+++ Tema3/src/parte4funciones/Ejercicio5.java
@@ -0,0 +1,46 @@
+package parte4funciones;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Ejercicio5 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int tabla[] = {1,2,3,4,5,2,4,3,2,1};
+		int num;
+		
+		System.out.println("Que numero quieres buscar");
+		num=sc.nextInt();
+		
+		int resultado[]=buscarTodos(tabla, num);
+		
+		System.out.println(Arrays.toString(resultado));
+		sc.close();
+	}
+
+	static int[] buscarTodos(int tabla[], int num) {
+		
+		int tamaño=0;
+		int siguiente=0;
+		
+		for(int contador=0;contador<tabla.length;contador++) {
+			if(tabla[contador]==num) {
+				tamaño++;
+			}
+		}
+		
+		int tablaNueva[] = new int[tamaño];
+		
+		for(int contador=0;contador<tabla.length;contador++) {
+			if(tabla[contador]==num) {
+				tablaNueva[0+siguiente]=contador;
+				siguiente++;
+			}
+		}
+		
+		return tablaNueva;
+	}
+}
