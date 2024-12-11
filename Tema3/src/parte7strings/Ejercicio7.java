da5e895be3e7a12af7ca172f1aae15083278f2f0
HugoDominguezF <hugo.dominguez@iesnervion.es>
2024-12-11 10:17:43 +0100

    ejercicios

--- Tema3/src/parte7strings/Ejercicio7.java
+++ Tema3/src/parte7strings/Ejercicio7.java
@@ -1,16 +1,37 @@
 package parte7strings;
 
+import java.util.Scanner;
+
 public class Ejercicio7 {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
 		
+		String frase;
 		
+		String palabra;
 		
+		int repetir=0;
 		
+		int pos;
 		
+		System.out.println("Escribe una frase completa");
+		frase= sc.nextLine();
 		
+		System.out.println("Escribe una palabra y te dire cuantas veces se repite");
+		palabra=sc.nextLine();
 		
+		pos=frase.indexOf(palabra);
+		
+		while(pos>=0) {
+			
+			repetir++;
+			pos=frase.indexOf(palabra, pos+1);
+			
+		}
+		
+		System.out.println("La palabra "+palabra+" se repite "+repetir+" veces");
 		
 		
 	}
